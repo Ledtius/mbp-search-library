912085eac9f072381a132c0fd22b17372b7d0ad9
Calet Devia <calebtius@gmail.com>
2025-07-18 11:32:28 -0500

    Working with the Google Books API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
+import { useState } from "react";
+console.log();
+const apiKey = import.meta.env.VITE_API_KEY;
 
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-
-    </>
-  )
+function writeSomething(e) {
+  const inputValue = e.value;
 }
 
-export default App
+function App() {
+  async function getBooks() {
+    try {
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=the+oldman+and+the+sea&time&printType=all&keyes&key=${apiKey}`
+      );
+
+      if (!response.ok) return new Error(`Error API: ${response.status}`);
+
+      const data = await response.json();
+
+      const bookItems = data.items;
+
+      bookItems.forEach((books) => {
+        console.log(books.volumeInfo.title);
+      });
+
+      console.log(data);
+    } catch (e) {
+      console.error("sss" + e);
+    }
+  }
+
+  getBooks();
+  console.log("sdfsfd");
+
+  return <></>;
+}
+
+export default App;
